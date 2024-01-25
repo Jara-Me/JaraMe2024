dcd740494ca0439416e6f26b88cc02d32b492646
박혜민 <phm6318@sookmyung.ac.kr>
2024-01-25 19:14:07 +0900

    Chore: 주석처리 복원
    테스트 목적으로 주석처리했던 부분 복원

--- BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/controller/JaraUsController.java
+++ BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/controller/JaraUsController.java
@@ -1,9 +1,9 @@
 package siliconDream.jaraMe.controller;
 
-//커밋 전에 취소할 부분 import jakarta.validation.Valid;
-//커밋 전에 취소할 부분 import lombok.RequiredArgsConstructor;
-//커밋 전에 취소할 부분 import org.springframework.security.core.Authentication;
-//커밋 전에 취소할 부분 import org.springframework.security.core.context.SecurityContextHolder;
+ import jakarta.validation.Valid;
+ import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,7 +16,7 @@ import siliconDream.jaraMe.domain.User;
 import siliconDream.jaraMe.dto.JaraUsDTO;
 import siliconDream.jaraMe.service.JaraUsService;
 import siliconDream.jaraMe.service.UserService;
-/*취소할 부분
+
 @Controller
 @RequiredArgsConstructor
 public class JaraUsController {
@@ -41,23 +41,4 @@ public class JaraUsController {
         model.addAttribute("jaraUsDTO", new JaraUsDTO());
         return "jaraUs/form";
     }
-/*주석처리 => createNewJaraUs 주석처리해놔서 여기서 인식못해서 에러남.
-
-    @PostMapping("/new-jaraUs")
-    public String newJaraUsSubmit(@Valid JaraUsDTO jaraUsDTO, Errors errors) {
-        if (errors.hasErrors()) {
-            return "jaraUs/form";
-        }
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        //수정한 부분
-        String userIdString = authentication.getName();
-        Long userId = Long.parseLong(userIdString);
-        User participant = userService.findUserByUserId(userId);
-
-        JaraUs newJaraUs = jaraUsService.createNewJaraUs(jaraUsDTO,userIdString);
-        //수정 전 : JaraUs newJaraUs = jaraUsService.createNewJaraUs(jaraUsDTO, String.valueOf(participant));
-        return "redirect:/jaraUs/" + newJaraUs.getJaraUsId();
-    }*/
-//취소할 부분 }
+ }
