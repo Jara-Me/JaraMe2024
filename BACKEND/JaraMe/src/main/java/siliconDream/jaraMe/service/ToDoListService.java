9d7186c2e05ca72c30aed1fe0f42e7d781694fdd
Hannah1011 <108389766+Hannah1011@users.noreply.github.com>
2024-02-01 21:21:30 +0900

    Update ToDoListService.java
    hasIncompleteTasks

--- BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/service/ToDoListService.java
+++ BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/service/ToDoListService.java
@@ -7,5 +7,5 @@ public interface ToDoListService {
     ToDoList createTesk(Long userId, String teskName); 
     void deleteTesk(Long todoListId);
     void toggleTeskStatus(Long todoListId);
-
+    boolean hasIncompleteTasks(Long userId);
 }
