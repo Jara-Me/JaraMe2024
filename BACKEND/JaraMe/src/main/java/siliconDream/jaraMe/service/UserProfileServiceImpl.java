9f655d5073e948d101920c04079c3b81cfd71c40
Hannah1011 <108389766+Hannah1011@users.noreply.github.com>
2024-02-05 08:01:14 +0900

    Update UserProfileServiceImpl.java
    프로필 정보 반환 
    -유저아이디, 닉네임 추가

--- BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/service/UserProfileServiceImpl.java
+++ BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/service/UserProfileServiceImpl.java
@@ -2,22 +2,27 @@ package siliconDream.jaraMe.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import siliconDream.jaraMe.domain.User;
 import siliconDream.jaraMe.dto.UserProfileInfoDTO;
 
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
 
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @Override
     public UserProfileInfoDTO getUserProfileInfo(Long userId) {
+        User user = userService.findUserByUserId(userId); // User 정보 조회
+
         UserProfileInfoDTO userProfileInfoDTO = new UserProfileInfoDTO();
+        userProfileInfoDTO.setUserId(user.getUserId());
+        userProfileInfoDTO.setNickname(user.getNickname());
+        userProfileInfoDTO.setProfileImage(user.getProfileImage());
         userProfileInfoDTO.setPoints(userService.getPoints(userId));
         userProfileInfoDTO.setPassTicket(userService.getPassTicket(userId));
         userProfileInfoDTO.setParticipatingJaraUsCount(userService.getParticipatingJaraUsCount(userId));
 
-        // 필요한 경우 추가 정보를 설정할 수 있습니다.
         return userProfileInfoDTO;
     }
 }
