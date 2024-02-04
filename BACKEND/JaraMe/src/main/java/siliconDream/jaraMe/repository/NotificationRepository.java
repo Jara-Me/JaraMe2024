379be780ca1e24d871058a91a1abb3a18dff4afd
Hannah1011 <108389766+Hannah1011@users.noreply.github.com>
2024-02-05 07:48:44 +0900

    Update NotificationRepository.java
    findByUserId 퀴리 생성 
    findByUserAndIsRead 변수 수정

--- BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/repository/NotificationRepository.java
+++ BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/repository/NotificationRepository.java
@@ -1,15 +1,19 @@
 package siliconDream.jaraMe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import siliconDream.jaraMe.domain.Notification;
+import siliconDream.jaraMe.domain.User;
 
 import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUserIdAndIsRead(Long userId, boolean isRead);
+    List<Notification> findByUserAndIsRead(User user, boolean isRead);
 
-    List<Notification> findByUserId(Long userId);
+    @Query("SELECT n FROM Notification n WHERE n.user.userId = :userId")
+    List<Notification> findByUserId(@Param("userId")Long userId);
 }
